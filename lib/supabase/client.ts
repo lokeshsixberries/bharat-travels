4b7438b0df2e5e1c4167e24497ad3a9fac3533ca
lokeshsixberries <lokesh@sixberries.com>
2025-07-26 03:51:13 +0530

    about client side issue fixed

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://opjswqdffbpmljqmaoox.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wanN3cWRmZmJwbWxqcW1hb294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxOTc2MDMsImV4cCI6MjA2ODc3MzYwM30.sjiTTj0IEUJjHKWGQOdmTwhV2rUkMnvrkA2oxrZzGlk"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
